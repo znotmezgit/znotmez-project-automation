d9ce70ebdcb871efb0d50863e6df728d79b49e8c
zitsmez <huynhletantai1@gmail.com>
2021-10-06 16:48:12 +0700

    Initial commt

---
+++ Exercise Files/02_05/start/test/SwitchToActiveWindow.java
@@ -0,0 +1,22 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class SwitchToActiveWindow {
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver", "/Users/meaghanlewis/Downloads/chromedriver");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://formy-project.herokuapp.com/switch-window");
+
+        WebElement name = driver.findElement(By.id("name"));
+        Actions actions = new Actions();
+        driver.switchTo().
+
+        driver.quit();
+    }
+}
